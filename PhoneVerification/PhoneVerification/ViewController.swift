fbfbf2c4fdb7f2a0215a1989693f7576235a5a8b
Menesidis <menesidis@gmail.com>
2018-10-01 22:12:09 +0300

    Init PhoneVerification

---
+++ PhoneVerification/PhoneVerification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhoneVerification
+//
+//  Created by Menesidis on 01/10/2018.
+//  Copyright © 2018 Menesidis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
