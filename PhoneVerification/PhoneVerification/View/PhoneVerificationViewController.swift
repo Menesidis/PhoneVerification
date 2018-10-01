b6f9274472b420360f83b113315b449c296f695f
Menesidis <menesidis@gmail.com>
2018-10-01 23:06:20 +0300

    feat(PhoneVerification): Add starting view

---
+++ PhoneVerification/PhoneVerification/View/PhoneVerificationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PhoneVerificationViewController.swift
+//  PhoneVerification
+//
+//  Created by Menesidis on 01/10/2018.
+//  Copyright © 2018 Menesidis. All rights reserved.
+//
+
+import UIKit
+
+class PhoneVerificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+     1. Fetch network calls from : https://pro.whitepages.com/developer/documentation/reverse-phone-api/
+     2. Add Pods
+     3. Add moya
+     4. Add Reactor Kit
+     5. Add RXSwift
+     6. Add Firebase
+     7. Add Fastlane
+     
+     */
+}
