203743dcf249aa4407d25c3c282386943a7d7734
Menesidis <menesidis@gmail.com>
2018-10-04 22:39:22 +0300

    feat(PhoneNumberVieController): Implement basic view

--- PhoneVerification/PhoneVerification/View/PhoneVerificationViewController.swift
+++ PhoneVerification/PhoneVerification/View/PhoneVerificationViewController.swift
@@ -8,22 +8,59 @@
 
 import UIKit
 
-class PhoneVerificationViewController: UIViewController {
-
+class PhoneVerificationViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    @IBOutlet weak var responseLabel: UILabel!
+    @IBOutlet weak var responseResultsLabel: UILabel!
+    
+    let network = Network()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = UIColor.green
+        phoneNumberTextField.delegate = self
+        
+        applyTheme()
+        localize()
+    }
+    
+    private func applyTheme(){
+        view.backgroundColor = UIColor.darkGray
+        phoneNumberLabel.textColor = UIColor.white
+        phoneNumberLabel.numberOfLines = 0
+        phoneNumberTextField.textColor = UIColor.white
+        responseLabel.textColor = UIColor.white
+        responseLabel.textColor = UIColor.white
+    }
+    
+    private func localize() {
+        phoneNumberLabel.text = "Please enter the phone you wish to find detailed information"
+        phoneNumberTextField.placeholder = "Phone number"
+        responseLabel.text = "Result :"
+        responseResultsLabel.text = "N/A"
     }
 
     /*
      1. Fetch network calls from : https://pro.whitepages.com/developer/documentation/reverse-phone-api/
-     2. Add Pods
-     3. Add moya
+     2. Deserialize parameter
+     3. Inject country code
      4. Add Reactor Kit
      5. Add RXSwift
      6. Add Firebase
      7. Add Fastlane
      
      */
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let phone = textField.text, phone.count > 4 {
+            network.fetchPersonData(phone: phone)
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
+
